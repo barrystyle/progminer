beaf9aecdc8d1b722782e8f0318dc562e0a91f39
HawyUkr <50444125+HawyUkr@users.noreply.github.com>
2019-05-31 14:14:47 +0300

    Fix typo

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,7 +1,7 @@
 hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=OFF CMAKE_USE_LIBSSH2=OFF CURL_CA_PATH=none)
 hunter_config(Boost VERSION 1.66.0)
 
-hunter_config(ethash VERSION 0.4.1
+hunter_config(ethash VERSION 0.4.5
     URL https://github.com/HawyUkr/ethash-1/archive/v0.4.5.tar.gz
     SHA1 12a7ad52809ca74f2a5e8f849408f0ebf795719c
 )
